4f8701868465064a1893dd8b5ad134f0bd70fb2e
trevor <tchung.jm@gmail.com>
2017-06-16 17:25:25 +0100

    course complete sans production deploy

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -7,8 +7,8 @@ const json = JSON.stringify(jsf(schema))
 
 fs.writeFile("./src/api/db.json", json, function(err){
   if (err) {
-    return console.log(chalk.red(err))
+    return console.log(chalk.red(err)) //eslint-disable-line no-console
   } else {
-    console.log(chalk.green("Mock data generated"))
+    console.log(chalk.green("Mock data generated")) //eslint-disable-line no-console
   }
 })
